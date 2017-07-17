7a4a7c47aac17880709b704db91ab438e4faaf07
gentok <gento.badger@gmail.com>
2017-07-17 03:13:23 -0700

    update

--- sources/plot_coef.R
+++ sources/plot_coef.R
@@ -6,6 +6,18 @@
 ## http://www.surefoss.org/dataanalysis/plotting-odds-ratios-aka-a-forrestplot-with-ggplot2/
 
 ## Need to import gktheme
+gktheme <-
+  theme(axis.text=element_text(size=10, colour="black"),
+        axis.title.x=element_text(size=12,face="bold", vjust=-1.5),
+        axis.title.y=element_text(size=12,face="bold", vjust=1.5),
+        plot.title=element_text(size=12,face="bold", vjust=2,hjust=0.5),
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
+        panel.grid.major = element_line(colour = "grey90"),
+        panel.grid.minor = element_line(colour = "grey90"),
+        panel.background = element_rect(fill=NA, colour="black", size=0.5, linetype=1),
+        legend.background = element_rect(fill=NA,colour=NA),
+        legend.position = c(0.75,0.68),
+        legend.key.width = unit(1.5, "cm"))
 
 plot_coef<-function(x, title = NULL, orderval="original", intercept=TRUE, direct=FALSE, odds=FALSE){
 #' @param title plot title (string)
